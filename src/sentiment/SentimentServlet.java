d80d1232a5af4a1d51963095e0dea65e65d278bd
Pazifik <shidori19@yahoo.com>
2015-09-15 08:52:08 +0200

    no message

--- src/sentiment/SentimentServlet.java
+++ src/sentiment/SentimentServlet.java
@@ -1,10 +1,6 @@
 package sentiment;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
