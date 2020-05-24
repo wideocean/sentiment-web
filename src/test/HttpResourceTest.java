d1158f8ad558103f1ea83a5c3df151ae3664b153
Pazifik Ngo <pazifikngo@gmail.com>
2020-05-24 15:54:33 +0200

    added more http tests

--- src/test/HttpResourceTest.java
+++ src/test/HttpResourceTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -11,29 +13,94 @@ import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import model.Pair;
 import model.Sentiment;
 import model.SentimentResult;
+import model.WordScorePair;
 
 class HttpResourceTest {
-	private final static String URL = "http://localhost:8080/sentiment-web";
+	private final String URL = "http://localhost:8080/sentiment-web";
+	private final String EN = "en";
 	private ObjectMapper mapper = new ObjectMapper();
 
 	@Test
-	void test() throws Exception {
+	void testSimple() throws Exception {
 		WebTarget webTarget = ClientBuilder.newClient().target(URL).path("sentiment");
 		Response response = webTarget//
-				.queryParam("text", "Heute ist ein schöner Tag")//
+				.queryParam("text", "Today is a good day.")//
 				.request(MediaType.APPLICATION_JSON)//
 				.get();
 
 		assertThat(response.getStatus()).isEqualTo(200);
 
 		String result = response.readEntity(String.class);
-		SentimentResult sentimentResult = mapper.readValue(result, SentimentResult.class);
+		System.out.println("Result: " + result);
 
+		SentimentResult sentimentResult = mapper.readValue(result, SentimentResult.class);
 		assertThat(sentimentResult.getLineNumber()).isEqualTo(1);
-		assertThat(sentimentResult.getLang()).isEqualTo("de");
+		assertThat(sentimentResult.getLang()).isEqualTo(EN);
 		assertThat(sentimentResult.getSentiment()).isEqualTo(Sentiment.POS);
+	}
+
+	@Test
+	void testWithKeywords() throws Exception {
+		WebTarget webTarget = ClientBuilder.newClient().target(URL).path("sentiment");
+		Response response = webTarget//
+				.path("keywords")//
+				.queryParam("text", "Today is not very good day.")//
+				.request(MediaType.APPLICATION_JSON)//
+				.get();
+
+		assertThat(response.getStatus()).isEqualTo(200);
+
+		String result = response.readEntity(String.class);
+		System.out.println("Result: " + result);
+
+		SentimentResult sentimentResult = mapper.readValue(result, SentimentResult.class);
+		assertThat(sentimentResult.getLineNumber()).isEqualTo(1);
+		assertThat(sentimentResult.getLang()).isEqualTo(EN);
+		assertThat(sentimentResult.getSentiment()).isEqualTo(Sentiment.NEG);
+		List<WordScorePair> keywords = sentimentResult.getKeywords();
+		assertThat(keywords).anySatisfy(wordScore -> {
+			assertThat(wordScore.getWord()).isEqualTo("not");
+			assertThat(wordScore.getNegation()).isTrue();
+		});
+		assertThat(keywords).anySatisfy(wordScore -> {
+			assertThat(wordScore.getWord()).isEqualTo("very");
+			assertThat(wordScore.getScore()).isEqualTo(1);
+		});
+		assertThat(keywords).anySatisfy(wordScore -> {
+			assertThat(wordScore.getWord()).isEqualTo("good");
+			assertThat(wordScore.getScorePair()).isEqualTo(new Pair(2, -1));
+		});
+	}
+
+	@Test
+	void testNoTextProvided() throws Exception {
+		WebTarget webTarget = ClientBuilder.newClient().target(URL).path("sentiment");
+		Response response = webTarget//
+				.request(MediaType.APPLICATION_JSON)//
+				.get();
+		assertThat(response.getStatus()).isEqualTo(400);
+
+		response = webTarget//
+				.queryParam("text", "")//
+				.request(MediaType.APPLICATION_JSON)//
+				.get();
+		assertThat(response.getStatus()).isEqualTo(400);
+
+		response = webTarget//
+				.path("keywords")//
+				.request(MediaType.APPLICATION_JSON)//
+				.get();
+		assertThat(response.getStatus()).isEqualTo(400);
+
+		response = webTarget//
+				.path("keywords")//
+				.queryParam("text", "")//
+				.request(MediaType.APPLICATION_JSON)//
+				.get();
+		assertThat(response.getStatus()).isEqualTo(400);
 	}
 
 }
